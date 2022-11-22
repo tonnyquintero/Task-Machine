c4d36acc5fcac25baba936af96b68a102356da0f
tonny187 <tonyoanthony@gmail.com>
2022-11-22 17:47:16 -0500

    Styling the dark components

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { ThemeContext } from '../Context/ThemeContext.jsx'
 import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
 function TodoCounter() {
 
-
+    const { darkMode } = useContext(ThemeContext);
     const { totalTodos, completedTodos } = React.useContext(TodoContext);
 
     return (
         <>
-            <h1>Bienvenidos a Task Machine 📓</h1>
-            <h2 className='TodoCounter'>
+            <h1 className={darkMode ? 'black' : 'white'}>Bienvenidos a Task Machine 📓</h1>
+            <h2 className={darkMode ? 'TodoCounter' : 'TodoCounterDark'}>
                 Has Completado {completedTodos} de {totalTodos} Tareas
             </h2>
         </>
